a57fb2d8f8899a3ab90fc745c78c1dc7c5e76427
Luiz Lopes <luizlopes12@outlook.com>
2021-05-05 12:07:04 -0300

    aplicação

---
+++ Crud/Crud/empregado.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crud
+{
+    class empregado : conexao
+    {
+        private string nome;
+        private string sobrenome;
+        private string cpf;
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string nome)
+        {
+            nome = this.nome;
+        }
+        public string getSobrenome()
+        {
+            return this.sobrenome;
+        }
+        public void setSobrenome(string sobrenome)
+        {
+            sobrenome = this.sobrenome;
+        }
+        public string getCpf()
+        {
+            return this.cpf;
+        }
+        public void setCpf(string cpf)
+        {
+            cpf = this.cpf;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
