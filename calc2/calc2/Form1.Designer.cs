a816204d06bb5e354a19f83bd1d1df3bab675807
Luiz Lopes <luizlopes12@outlook.com>
2021-05-10 10:37:00 -0300

    Update Form1.Designer.cs

--- calc2/calc2/Form1.Designer.cs
+++ calc2/calc2/Form1.Designer.cs
@@ -82,6 +82,7 @@
             this.btnLimpar.TabIndex = 31;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // btn0
             // 
