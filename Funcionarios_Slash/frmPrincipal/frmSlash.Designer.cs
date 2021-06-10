71f245bbb851b7222f0d6d1b8f7da20c9fec839a
Luiz Lopes <luizlopes12@outlook.com>
2021-06-10 20:42:01 -0300

    sla

---
+++ Funcionarios_Slash/frmPrincipal/frmSlash.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace frmPrincipal
+{
+    partial class frmSlash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pgbCarregar = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18.25F);
+            this.label1.Location = new System.Drawing.Point(111, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD Funcionários";
+            // 
+            // pgbCarregar
+            // 
+            this.pgbCarregar.Location = new System.Drawing.Point(116, 203);
+            this.pgbCarregar.Name = "pgbCarregar";
+            this.pgbCarregar.Size = new System.Drawing.Size(248, 23);
+            this.pgbCarregar.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmSlash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 372);
+            this.Controls.Add(this.pgbCarregar);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSlash";
+            this.Text = "frmSlash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pgbCarregar;
+        private System.Windows.Forms.Timer timer;
+    }
+}
