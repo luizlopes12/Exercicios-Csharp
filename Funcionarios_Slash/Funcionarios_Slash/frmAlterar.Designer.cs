7fb133344c1baa327a903a19fe6cf4b84bc30fe4
Luiz Lopes <luizlopes12@outlook.com>
2021-06-19 19:26:03 -0300

    continuação

---
+++ Funcionarios_Slash/Funcionarios_Slash/frmAlterar.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Funcionarios_Slash
+{
+    partial class frmAlterar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.mskCpf = new System.Windows.Forms.MaskedTextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.dgvFuncionarioAlterar = new System.Windows.Forms.DataGridView();
+            this.btnConsultarRg = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.mskConsultarRg = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarioAlterar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(63, 474);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "RG";
+            // 
+            // mskCpf
+            // 
+            this.mskCpf.Enabled = false;
+            this.mskCpf.Location = new System.Drawing.Point(97, 467);
+            this.mskCpf.Mask = "99.999.999-9";
+            this.mskCpf.Name = "mskCpf";
+            this.mskCpf.Size = new System.Drawing.Size(154, 20);
+            this.mskCpf.TabIndex = 29;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(97, 441);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(154, 20);
+            this.txtSobrenome.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 444);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Salario";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(97, 389);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(154, 20);
+            this.txtNome.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 392);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Nome";
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(327, 352);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(139, 44);
+            this.btnAlterar.TabIndex = 24;
+            this.btnAlterar.Text = "Alterar dados";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // dgvFuncionarioAlterar
+            // 
+            this.dgvFuncionarioAlterar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFuncionarioAlterar.Location = new System.Drawing.Point(29, 157);
+            this.dgvFuncionarioAlterar.Name = "dgvFuncionarioAlterar";
+            this.dgvFuncionarioAlterar.Size = new System.Drawing.Size(437, 189);
+            this.dgvFuncionarioAlterar.TabIndex = 23;
+            // 
+            // btnConsultarRg
+            // 
+            this.btnConsultarRg.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConsultarRg.Location = new System.Drawing.Point(327, 85);
+            this.btnConsultarRg.Name = "btnConsultarRg";
+            this.btnConsultarRg.Size = new System.Drawing.Size(139, 35);
+            this.btnConsultarRg.TabIndex = 22;
+            this.btnConsultarRg.Text = "Consultar RG";
+            this.btnConsultarRg.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(25, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 20);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "consultar por RG:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(62, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 29);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Alterar dados dos empregados";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(97, 415);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(154, 20);
+            this.textBox1.TabIndex = 32;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 418);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Data";
+            // 
+            // mskConsultarRg
+            // 
+            this.mskConsultarRg.Enabled = false;
+            this.mskConsultarRg.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.mskConsultarRg.Location = new System.Drawing.Point(171, 89);
+            this.mskConsultarRg.Mask = "99.999.999-9";
+            this.mskConsultarRg.Name = "mskConsultarRg";
+            this.mskConsultarRg.Size = new System.Drawing.Size(141, 24);
+            this.mskConsultarRg.TabIndex = 33;
+            // 
+            // frmAlterar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 529);
+            this.Controls.Add(this.mskConsultarRg);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.mskCpf);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.dgvFuncionarioAlterar);
+            this.Controls.Add(this.btnConsultarRg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAlterar";
+            this.Text = "Alterar dados";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarioAlterar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mskCpf;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.DataGridView dgvFuncionarioAlterar;
+        private System.Windows.Forms.Button btnConsultarRg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox mskConsultarRg;
+    }
+}
