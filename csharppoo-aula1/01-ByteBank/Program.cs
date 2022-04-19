4346e26e0209f8132a67ec133cf127e24e81810c
Luiz Lopes <luizlopes12@outlook.com>
2022-04-18 22:10:54 -0300

    POO

---
+++ csharppoo-aula1/01-ByteBank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "uepa";
+            conta.agencia = 033;
+            conta.numero = 111221;
+            conta.saldo = 22.66;
+
+            Console.WriteLine($"{conta.titular}{conta.agencia}{conta.numero}{conta.saldo}");
+            Console.ReadLine();
+        }
+    }
+}
